b19a84d65aff6b41b5db5e94aae28905b51b2391
Stevan Aji <stevanadjie@gmail.com>
2021-12-08 01:51:11 +0700

    Fixing admin lte done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import "../styles/vendor.scss"
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,8 +14,16 @@ class MyDocument extends Document {
         <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
+
+          {/* <link rel="stylesheet" href="/_next/static/vendor.scss"/> */}
+          <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"/>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"/>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"/>
+          {/* This provides all admin-lte functionality - we copied the files to our static directory above */}
+          <script src="/static/adminlte.js"/>
         </Head>
-        <body>
+        <body className="sidebar-mini">
           <Main />
           <NextScript />
         </body>
